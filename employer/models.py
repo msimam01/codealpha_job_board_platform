41d726b034b07e9112c0bb2bc4689ebd17db0009
Abubakar Ibrhim Umar <abubakaribrahim30012@gmail.com>
2025-01-06 16:25:39 +0100

    initial commit

---
+++ employer/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+# Create your models here.
+class Job(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    company = models.CharField(max_length=100)
+    location = models.CharField(max_length=100)
+    posted_date = models.DateField(auto_now_add=True)
+    deadline = models.DateField()
+    
+    def __str__(self):
+        return self.title, self.description
+    
+
---
+++ job_seeker/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+from django.contrib.auth.models import User
+from employer.models import Job
+
+# Create your models here.
+class Application(models.Model):
+    job = models.ForeignKey(to=Job, related_name='job', on_delete=models.CASCADE)
+    user = models.ForeignKey(to=User, related_name='user', on_delete=models.CASCADE)
+    resume = models.FileField(upload_to='resume/')
+    application_date = models.DateField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.job, self.user
+    
+    
+    
